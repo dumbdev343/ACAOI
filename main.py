74ccd9ff4fb928daa04710be09a7ef39aeac9794
DumbDev343 <62670550+dumbdev343@users.noreply.github.com>
2026-06-03 20:04:07 +1200

    add basic linux detection and rename to acaoi

--- main.py
+++ main.py
@@ -5,87 +5,98 @@ import platform
 
 
 ascii = r"""
-    _   ___   ___      _____ 
-   /_\ / __| /_\ \    / /_ _|
-  / _ \ (__ / _ \ \/\/ / | | 
- /_/ \_\___/_/ \_\_/\_/ |___|
-                             
+    _   ___   _   ___ ___ 
+   /_\ / __| /_\ / _ \_ _|
+  / _ \ (__ / _ \ (_) | | 
+ /_/ \_\___/_/ \_\___/___|
+                          
 """
-
+lnx = False
 if platform.system() == "Windows":
     rw = True
     os.system("cls")
     print("Running on Windows.")
     time.sleep(0.7)
+elif platform.system() == "Linux":
+    lnx = True
+    print("Running on Linux")
 else:
     rw = False
-    print("Sorry, this program only runs on Windows.")
+    print("Sorry, this program only runs on Windows and Linux.")
+
+if lnx == True:
+    os.system("clear")
+    print(ascii)
+    print("Welcome to ACAOI (Automated Common Apps OS Installer)")
+    pkg = input("What would you like to do? 1. Install your own package (Using your Distro's package manager) 2. Quit")
+    if "1" in pkg:
+        print("Installing custom package selected.")
 
 if rw == True:
     os.system("cls")
     print(ascii)
-    print("Welcome to ACAWI (Automated Common Apps Windows Installer)")
+    print("Welcome to ACAOI (Automated Common Apps OS Installer)")
     ans = input("What would you like to do? 1.Install ALL Common Apps Automaticaly 2. Install Only Some Common Applications 3. Install a Web Browser 4. Install your own app 5. Do Nothing and Exit? ")
-
-if "1" in ans:
-    print("1. Selected. Installing all common apps with log.")
-    confirm = input("Are you sure you want to do this? This will install 40+ apps on your Computer eg: Firefox, Chrome, Brave, WireShark, Spotify etc: ")
-    if "yes" in confirm:
-        os.system(f"winget import host.txt --accept-source-agreements --accept-package-agreements")
-    else:
-        print("Something other than yes was entered, exiting...")
+    if "1" in ans:
+        print("1. Selected. Installing all common apps with log.")
+        confirm = input("Are you sure you want to do this? This will install 40+ apps on your Computer eg: Firefox, Chrome, Brave, WireShark, Spotify etc: ")
+        if "yes" in confirm:
+            os.system(f"winget import host.txt --accept-source-agreements --accept-package-agreements")
+        else:
+            print("Something other than yes was entered, exiting...")
 
     
+    if "2" in ans:
+        print("2. Selected. Installing Set of only some common apps")
+        os.system("winget import simple.txt")
+        browser = input('Which browser do you want to install? 1. Firefox 2. Brave 3. Google Chrome 4. Chromium 5. DeGoogled Chromium 6. All: 7: None:  ')
+        if "1" in browser:
+            print("Firefox selected")
+            os.system("winget install Mozilla.Firefox -h --accept-source-agreements --accept-package-agreeements")
+        if "2" in browser:
+            print("Brave selected.")
+            os.system("winget install Brave.Brave -h --accept-source-agreements --accept-package-agreements")
+        if "3" in browser:
+            print("Google Chrome selected.")
+            os.system("winget install Google.Chrome -h --accept-source-agreements --accept-package-agreements")
+        if "4" in browser:
+            os.system("winget install Hibbiki.Chromium -h --accept-package-agreements --accept-source-agreements")
+        if "5" in browser:
+            print("DeGoogled Chromium selected.")
+            os.system("winget install --id eloston.ungoogled-chromium -h --accept-package-agreements --accept-source-agreements")
+        if "6" in browser:
+            print("All selected.")
+            os.system("winget install Mozilla.Firefox Brave.Brave Google.Chrome Hibbiki.Chromium eleston.ungoogled-chromium --slient --accept-package-agreements --accept-source-agreements")
+        else:
+            print("None was selected, exiting...")
+    if "3" in ans:
+        browser = input('Which browser do you want to install? 1. Firefox 2. Brave 3. Google Chrome 4. Chromium 5. DeGoogled Chromium 6. All: ')
+        if "1" in browser:
+            print("Firefox selected")
+            os.system("winget install Mozilla.Firefox -h --accept-source-agreements --accept-package-agreeements")
+        if "2" in browser:
+            print("Brave selected.")
+            os.system("winget install Brave.Brave -h --accept-source-agreements --accept-package-agreements")
+        if "3" in browser:
+            print("Google Chrome selected.")
+            os.system("winget install Google.Chrome -h --accept-source-agreements --accept-package-agreements")
+        if "4" in browser:
+            print("Chromium selected.")
+            os.system("winget install Hibbiki.Chromium -h --accept-package-agreements --accept-source-agreements")
+        if "5" in browser:
+            print("DeGoogled Chromium selected.")
+            os.system("winget install --id eloston.ungoogled-chromium -h --accept-package-agreements --accept-source-agreements")
+        if "6" in browser:
+            print("All selected.")
+            os.system("winget install Mozilla.Firefox Brave.Brave Google.Chrome Hibbiki.Chromium eleston.ungoogled-chromium --slient --accept-package-agreements --accept-source-agreements")
+        else: 
+            print("None was selected, exiting")
 
-if "2" in ans:
-    print("2. Selected. Installing Set of only some common apps")
-    os.system("winget import simple.txt")
-    browser = input('Which browser do you want to install? 1. Firefox 2. Brave 3. Google Chrome 4. Chromium 5. DeGoogled Chromium 6. All: ')
-    if "1" in browser:
-        print("Firefox selected")
-        os.system("winget install Mozilla.Firefox -h --accept-source-agreements --accept-package-agreeements")
-    if "2" in browser:
-        print("Brave selected.")
-        os.system("winget install Brave.Brave -h --accept-source-agreements --accept-package-agreements")
-    if "3" in browser:
-        print("Google Chrome selected.")
-        os.system("winget install Google.Chrome -h --accept-source-agreements --accept-package-agreements")
-    if "4" in browser:
-        os.system("winget install Hibbiki.Chromium -h --accept-package-agreements --accept-source-agreements")
-    if "5" in browser:
-        print("DeGoogled Chromium selected.")
-        os.system("winget install --id eloston.ungoogled-chromium -h --accept-package-agreements --accept-source-agreements")
-    if "6" in browser:
-        print("All selected.")
-        os.system("winget install Mozilla.Firefox Brave.Brave Google.Chrome Hibbiki.Chromium eleston.ungoogled-chromium --slient --accept-package-agreements --accept-source-agreements")
-    else:
-        print("None was selected, exiting...")
-if "3" in ans:
-    browser = input('Which browser do you want to install? 1. Firefox 2. Brave 3. Google Chrome 4. Chromium 5. DeGoogled Chromium 6. All: ')
-    if "1" in browser:
-        print("Firefox selected")
-        os.system("winget install Mozilla.Firefox -h --accept-source-agreements --accept-package-agreeements")
-    if "2" in browser:
-        print("Brave selected.")
-        os.system("winget install Brave.Brave -h --accept-source-agreements --accept-package-agreements")
-    if "3" in browser:
-        print("Google Chrome selected.")
-        os.system("winget install Google.Chrome -h --accept-source-agreements --accept-package-agreements")
-    if "4" in browser:
-        os.system("winget install Hibbiki.Chromium -h --accept-package-agreements --accept-source-agreements")
-    if "5" in browser:
-        print("DeGoogled Chromium selected.")
-        os.system("winget install --id eloston.ungoogled-chromium -h --accept-package-agreements --accept-source-agreements")
-    if "6" in browser:
-        print("All selected.")
-        os.system("winget install Mozilla.Firefox Brave.Brave Google.Chrome Hibbiki.Chromium eleston.ungoogled-chromium --slient --accept-package-agreements --accept-source-agreements")
-    else: 
-        print("None was selected, exiting")
+    if "4" in ans:
+        app = input("What app do you want to install? (Using WinGet and ID (eg: Valve.Steam) ): ")
+        time.sleep(0.3)
+        os.system(f"winget install {app}")
 
-if "4" in ans:
-    app = input("What app do you want to install? (Using WinGet and ID (eg: Valve.Steam) ): ")
-    time.sleep(0.3)
-    os.system(f"winget install {app}")
 
 
 
